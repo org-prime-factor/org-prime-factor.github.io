1f4ead2cd838c6404e91803b5b4779cf5a9e4f8f
Shahed <shahed@King-Ali.local>
2026-04-12 19:33:13 +0530

    fix: remove sitemap, update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://shahed.dev',
-  integrations: [
-    mdx(),
-    react(),
-    tailwind({ applyBaseStyles: false }),
-    sitemap(),
-  ],
+  site: "https://org-prime-factor.github.io",
+  integrations: [mdx(), react(), tailwind({ applyBaseStyles: false })],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
+      theme: "github-dark",
       wrap: true,
     },
   },
